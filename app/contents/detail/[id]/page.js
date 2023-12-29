a8a9124fceda78da49e7af7eba3167f165afe75c
Mert Baykal <mert.baykal@trive.com>
2023-12-29 15:44:53 +0300

    Add back btn

--- app/contents/detail/[id]/page.js
+++ app/contents/detail/[id]/page.js
@@ -1,12 +1,14 @@
-"use client";
-
+"use client"
 import { Api } from "@/app/core/api/contents";
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Page({ params }) {
 
   const [data, setDatas] = useState();
   const { id } = params;
+
+  const router = useRouter();
 
   useEffect(() => {
     const response = Api.readContent(id);
@@ -14,13 +16,16 @@ export default function Page({ params }) {
     setDatas(response);
   }, []);
 
-  return <div>
-    <p>detay sayfası</p>
-    <div className="text-center">
-      <div dangerouslySetInnerHTML={{ __html: data?.title }}></div>
-      <div dangerouslySetInnerHTML={{ __html: data?.description }}></div>
-      <div className="text-end text-xs p-1">{data?.user.name} - {data?.createdDate} </div>
+  return (
+    <div className="p-4">
+      <button className="text-white bg-black p-2 rounded-md" onClick={() => router.back()}>
+        Geri Dön
+      </button>
+      <div className="text-center">
+        <div dangerouslySetInnerHTML={{ __html: data?.title }}></div>
+        <div dangerouslySetInnerHTML={{ __html: data?.description }}></div>
+        <div className="text-end text-xs p-1">{data?.user.name} - {data?.createdDate} </div>
+      </div>
     </div>
-    
-  </div>
+  );
 }
